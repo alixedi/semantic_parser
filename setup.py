dc8246ad209854f31ea7a596f9ba1f8fa49fe7a1
Ali Zaidi <alixedi@bitswits.com>
2013-11-20 21:37:00 +0500

    Added __init__.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Semantic Parser',
+    version='0.1',
+    packages=['semantic_parser',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
